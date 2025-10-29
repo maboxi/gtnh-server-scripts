2a5085a8d30f5cd72092dab83cb47193145eda70
Maximilian Pöllinger <maxi.poellinger@gmail.com>
2025-10-29 22:38:13 +0100

    script for retreiving the remaining scripts

---
+++ get_scripts.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -eu
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SCRIPTS_REPOSITORY="https://github.com/maboxi/gtnh-server-scripts.git"
+
+if [ $# -ge 1 ]; then
+    SCRIPTS_DIR="$1"
+    echo "Using custom scripts directory: $SCRIPTS_DIR"
+fi
+
+SCRIPTS_DIR="${SCRIPTS_DIR:-"${SCRIPT_DIR}/scripts"}"
+
+if [ -d "$SCRIPTS_DIR" ]; then
+    echo "Removing existing scripts directory: $SCRIPTS_DIR"
+    rm -r "$SCRIPTS_DIR"
+fi
+
+if which -q git; then
+    echo "Cloning scripts repository '$SCRIPTS_REPOSITORY' into '$SCRIPTS_DIR'..."
+    git clone "$SCRIPTS_REPOSITORY" "$SCRIPTS_DIR"
+else
+    echo "Error: scripts retreival without git is not implemented yet!"
+    exit 1
+fi
+
+echo "Done. Scripts are available in '$SCRIPTS_DIR'."
