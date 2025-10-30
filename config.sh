c5e5bc82aa8383e11907c1e716531ef1bb86a4fe
Maximilian Pöllinger <maxi.poellinger@gmail.com>
2025-10-30 01:35:18 +0100

    config.sh: use simpler SCRIPT_DIR determination, removed sourcing test

--- config.sh
+++ config.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
-if [ ${BASH_SOURCE[0]} = ${0} ]; then
-        echo "This script can only be sourced!"
-	exit 1
-fi
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 RUN_DIR="run"
 RUN_DIR_CONTAINER_PATH="/opt/server"
 JAVA_CONTAINER_IMAGE="amazoncorretto:25"
 
-ROOT_DIR="$(realpath "$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/.." )"
+ROOT_DIR="$(realpath "$SCRIPT_DIR/..")"
 
 RUN_DIR_HOST_PATH="$ROOT_DIR/$RUN_DIR"
